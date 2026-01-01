4415679901bd67c74ce3d1c0dc4c93e8b508d69e
Matthew Hinkleman <matthewjhinkleman@gmail.com>
2026-01-01 11:06:19 -0800

    Updated death screen and added level clear screens
    Death Screen now shows your coins

---
+++ Game/scenes/level_1_clear.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@export var next_level: PackedScene
+@onready var total_coins: Label = $TotalCoins
+@onready var level_coins: Label = $LevelCoins
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Music.play_music(preload("res://assets/music/flowerbed_fields.ogg"))
+	level_coins.text = "Level Coins: " + str(GameManager.level_score) + "/25"
+
+func _process(_delta):
+	# Detect input to start game
+	if Input.is_action_just_pressed("start"):
+		start_next_level()
+
+func start_next_level():
+	# Load the first level
+	get_tree().change_scene_to_packed(next_level)
--- Game/scripts/death_screen.gd
+++ Game/scripts/death_screen.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
+@onready var total_coins: Label = $TotalCoins
+@onready var level_coins: Label = $LevelCoins
+
+func _ready():
+	total_coins.text = "Total Coins: " + str(GameManager.total_score)
+	level_coins.text = "Current Level Coins: " + str(GameManager.level_score)
+
 func _process(_delta):
 	if Input.is_action_just_pressed("start"):
 		restart_level()
